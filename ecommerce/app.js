e3a3b1e1b8e99647023cea12aaaf0e0cb5a75ec8
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-13 06:04:49 +0300

    Added a 404 page

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = 3000;
 
@@ -16,7 +17,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res) => {
-  res.status(404).send('<h1>No page found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 const server = http.createServer(app);
 
