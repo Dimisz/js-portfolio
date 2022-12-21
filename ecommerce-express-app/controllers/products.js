0ecc0121fba9f066b53c0e0f5adc18af4dd687e5
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-21 10:36:55 +0800

    Added product model

--- ecommerce-express-app/controllers/products.js
+++ ecommerce-express-app/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
   // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
@@ -12,11 +12,13 @@ exports.getAddProduct = (req, res) => {
 };
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title : req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', {
     prods: products, 
     pageTitle: 'Products Page', 
