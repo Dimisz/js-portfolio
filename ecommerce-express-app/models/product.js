fdcccfda088ba991e5f4974ce5add64e2796c490
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-21 15:00:55 +0800

    Housekeeping

--- ecommerce-express-app/models/product.js
+++ ecommerce-express-app/models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +9,35 @@ module.exports = class Product {
   }
 
   save(){
-    products.push(this);
+    const productPath = path.join(
+                                  path.dirname(process.mainModule.filename),
+                                  'data',
+                                  'products.json'
+     );
+     fs.readFile(productPath, (err, fileContent) => {
+        let products = [];
+        if(!err){
+          products = JSON.parse(fileContent);
+        }
+        products.push(this);
+        fs.writeFile(productPath, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+     });
   }
 
-  static fetchAll(){
-    return products;
+  static fetchAll(cb){
+    const productPath = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(productPath, (err, fileContent) => {
+      if (err){
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 }
